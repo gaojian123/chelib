192c274a3963f1d4af47c100f88c721cd56e7b9b
gaojian123 <gj949326797@163.com>
2015-11-05 11:03:31 +0800

    修改isDebug为isLibDebug

--- cheLib/src/com/easier/library/util/Constants.java
+++ cheLib/src/com/easier/library/util/Constants.java
@@ -1,5 +1,5 @@
 package com.easier.library.util;
 
 public class Constants {
-	public static final boolean isDebug = true;
+	public static final boolean isLibDebug = true;
 }
--- cheLib/src/com/easier/library/util/LogUtil.java
+++ cheLib/src/com/easier/library/util/LogUtil.java
@@ -3,63 +3,63 @@ package com.easier.library.util;
 import android.util.Log;
 
 /**
- * 日志输出工具（�?�过Constants.isDebug控制是否输出日志�?
+ * 日志输出工具（�?�过Constants.isLibDebug控制是否输出日志�?
  * @author gaojian
  *
  */
 public class LogUtil {
 	
 	public static void i(String tag,String msg){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.i(tag, msg);
 		}
 	}
 	public static void i(String tag,String msg,Throwable tr){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.i(tag, msg, tr);
 		}
 	}
 	
 	public static void e(String tag,String msg){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.e(tag, msg);
 		}
 	}
 	public static void e(String tag,String msg,Throwable tr){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.e(tag, msg, tr);
 		}
 	}
 	
 	public static void v(String tag,String msg){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.v(tag, msg);
 		}
 	}
 	public static void v(String tag,String msg,Throwable tr){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.v(tag, msg, tr);
 		}
 	}
 	
 	public static void w(String tag,String msg){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.w(tag, msg);
 		}
 	}
 	public static void w(String tag,String msg,Throwable tr){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.w(tag, msg, tr);
 		}
 	}
 	
 	public static void d(String tag,String msg){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.d(tag, msg);
 		}
 	}
 	public static void d(String tag,String msg,Throwable tr){
-		if (Constants.isDebug) {
+		if (Constants.isLibDebug) {
 			Log.d(tag, msg, tr);
 		}
 	}
